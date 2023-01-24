5cd952e4c394abed68ee5596894010a4f8f988d3
apollosport <apollosport84@gmail.com>
2023-01-24 09:16:52 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/logIn";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/login"
+          element={!user ? <LogIn setUser={setUser} /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignUp setUser={setUser} /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
