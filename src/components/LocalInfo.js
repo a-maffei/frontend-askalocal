252dfd458950d5849bc9d5094099bfe5fc593ab2
Apollosport <apollosport84@gmail.com>
2023-01-26 15:49:45 +0100

    small changes to home

--- src/components/LocalInfo.js
+++ src/components/LocalInfo.js
@@ -1,42 +1,37 @@
-import React , {useState , useEffect} from "react";
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
+export default function LocalInfo() {
+  // const { id } = useParams()
+  const { id } = useParams();
+  const [local, setLocal] = useState([]);
+  const url = `https://backend-askalocal.onrender.com/local/${id}`;
+  const url2 = `http://localhost:8080/local/${id}`;
 
+  console.log("bamm ", id);
 
-export default function LocalInfo(){
-    // const { id } = useParams()
-    const {id} = useParams()
-    const [local , setLocal] = useState([])
+  const fetchData = async () => {
+    console.log("test", id);
+    const result = await axios.get(url);
+    setLocal(result.data.local);
+    console.log(result.data);
+  };
 
-    console.log('bamm ',id) 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {  
-        console.log('test',id)          
-        const result = await axios.get(`https://backend-askalocal.onrender.com/local/${id}`)
-        setLocal(result.data.local)
-        
-    };
+  // const oneLocal = local.find((local) => local._id === id)
 
-    useEffect(() => {        
-        fetchData()
-    }, [])
+  console.log(id);
 
-    
-
-   // const oneLocal = local.find((local) => local._id === id)
-
-    // console.log(oneLocal)
-  
-    return(
-        <div>
-           <h1>Local Info</h1>
-           <img src={local.pic} style={{width: "200px"}}/>
-           <h2>{local.firstname}</h2>
-           <p>{local.bio}</p>
-        
-           
-           
-           </div>
-    )
+  return (
+    <div>
+      <h1>Local Info</h1>
+      <img src={local?.pic} style={{ width: "200px" }} />
+      <h2>{local?.firstname}</h2>
+      <p>{local?.bio}</p>
+    </div>
+  );
 }
