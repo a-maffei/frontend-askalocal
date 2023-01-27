b488ce0aabea43e5960b4110ef5330be466697df
Antonio Macan <macan@Antonios-MacBook-Air.local>
2023-01-27 14:53:57 +0100

    erge conflict

--- src/components/LocalInfo.js
+++ src/components/LocalInfo.js
@@ -43,32 +43,8 @@ export default function LocalInfo() {
 
   // console.log(oneLocal)
 
-  return local ? (
+  return (
     <div>
-      <h1>Local Info</h1>
-      <div className="first-section">
-        <img src={local?.pic} style={{ width: "200px" }} />
-        <div>
-          <h2>{local?.firstname}</h2>
-          <p>{local?.bio}</p>
-        </div>
-      </div>
-      <h2>{local?.firstname} Offers:</h2>
-      <div className="offers">
-        <h3>{local.categories?.emailP.category}</h3>
-        <p>{local.categories?.emailP?.textfield}</p>
-        <p>{local.categories?.emailP?.price} €</p>
-      </div>
-      <div className="offers">
-        <h3>{local.categories?.interviewP?.category}</h3>
-        <p>{local.categories?.interviewP?.textfield}</p>
-        <p>{local.categories?.interviewP?.price} €</p>
-      </div>
-      <div className="offers">
-        <h3>{local.categories?.appointmentP?.category}</h3>
-        <p>{local.categories?.appointmentP?.textfield}</p>
-        <p>{local.categories?.appointmentP?.price} €</p>
-      </div>
       <div className="reviews">
         <h2>Reviews</h2>
       </div>
@@ -102,7 +78,5 @@ export default function LocalInfo() {
       </div>
       ;
     </div>
-  ) : (
-    "Loading"
   );
 }
